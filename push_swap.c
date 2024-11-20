dd03a2d6ea4ed7cab4d8caad39c0bd33ea6db5c8
Crayfe <crayfe@gmail.com>
2024-11-20 23:57:11 +0100

    Testing imputs

--- push_swap.c
+++ push_swap.c
@@ -3,30 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: crayfe <crayfe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:44:08 by cayuso-f          #+#    #+#             */
-/*   Updated: 2024/11/18 17:18:23 by cayuso-f         ###   ########.fr       */
+/*   Updated: 2024/11/20 21:38:07 by crayfe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "push_swap.h"
-int	int_arg_checker(int argc, char **argv)
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int	isnum(char *s)
 {
 	int	i;
-	int	len;
+	int	num;
 
-	i = 1;
-	j = 0;
-	while (i++ < argc)
+	if (!s)
+		return (0);
+	i = 0;
+	num = 0;
+	while (s[i])
 	{
-		len = ft_strlen(argv[i]);
-		while (j++ < len)
-			if(!ft_isdigit(argv[i][j]))
+		if (i == 0)
+		{
+			if(ft_isdigit(s[i]) || (s[i] == '-' && ft_isdigit(s[1])))
+				num = 1;
+			else
 				return (0);
+		}
+		else
+			if (!ft_isdigit(s[i]))
+				return (0);
+		i++;
 	}
-	return (1);
+	return (num);
 }
+int	isint(char *s)
+{
+	long num;
 
+	num = ft_atol(s);
+	if (num >= -2147483648 && num <= 2147483647)
+		return (1);
+	else
+		return (0);
+}
+int	int_checker(char *arg)
+{
+	if (!isnum(arg))
+		return (0);
+	if (!isint(arg))
+		return (0);
+	return(1);
+}
+/*
 t_list	ft_split_int(int argc, char **argv)
 {
 	return (0);
@@ -42,5 +72,12 @@ int	main(int argc, char **argv)
 	}
 	else
 		ft_printf("Error\n");
+	return (0);
+}
+*/
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		printf("is %s an int? %i\n", argv[1], int_checker(argv[1]));
 	return (0);
 }
