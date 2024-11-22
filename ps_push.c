511900998b87b0025fe1ed7661395d1577b1f6a6
Crayfe <crayfe@gmail.com>
2024-11-22 23:47:04 +0100

    implemented swap and push

--- ps_push.c
+++ ps_push.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ps_push.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cayuso-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -9,18 +9,30 @@
 /*   Updated: 2024/11/18 17:18:23 by cayuso-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+#include "push_swap.h"
 
-t_list	*load_stack(int argc, char **argv);
-void	print_stack(t_list *stack);
-void	swap_a(t_list **stack_a);
-void	swap_b(t_list **stack_b);
-void	swap_a_b(t_list **stack_a, t_list **stack_b);
-void	push_a(t_list **stack_b, t_list **stack_a);
-void	push_b(t_list **stack_a, t_list **stack_b);
-void	rotate_a(t_list **stack_a);
-void	rotate_b(t_list **stack_b);
-void	rotate_a_b(t_list **stack_a, t_list **stack_b);
-void	rev_rotate_a(t_list **stack_a);
-void	rev_rotate_b(t_list **stack_b);
-void	rev_rotate_a_b(t_list **stack_a, t_list **stack_b);
+int	push(t_list **stack_src, t_list **stack_dst)
+{
+	t_list	*aux;
+
+	if (stack_src[0])
+	{
+		aux = stack_src[0];
+		stack_src[0] = stack_src[0]->next;
+		ft_lstadd_front(stack_dst, aux);
+		return (1);
+	}
+	return (0);
+}
+
+void	push_a(t_list **stack_b, t_list **stack_a)
+{
+	if (push(stack_b, stack_a))
+		ft_printf("pa\n");
+}
+
+void	push_b(t_list **stack_a, t_list **stack_b)
+{
+	if (push(stack_a, stack_b))
+		ft_printf("pb\n");
+}
